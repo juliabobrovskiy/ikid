7e635e0ad2f96a3dbb3ef405e3e13ccacf31a10b
Julia Bobrovskiy <jvb8@uw.edu>
2018-10-23 14:33:21 -0700

    add starter code

---
+++ ikid/ikid/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  iKid
+//
+//  Created by Julia Bobrovskiy on 10/23/18.
+//  Copyright © 2018 Julia Bobrovskiy. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ikid/ikid/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  iKid
+//
+//  Created by Julia Bobrovskiy on 10/23/18.
+//  Copyright © 2018 Julia Bobrovskiy. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
